3431b3467f70ab7dca415aaadc9a064f172863f4
Simen Daehlin <simen.daehlin@strapi.io>
2024-06-07 10:32:13 +0100

    Removed npm clean from setup to make it work on start

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className=''>Welcome to Strapi LMS</h1>
-      <Button variant={ "secondary" }>Test</Button>
+      <Button variant="secondary">Test</Button>
     </>
   )
 }
