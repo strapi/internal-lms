bf0cd417de28c7a37a8915c7b2025a2e6402039b
Simen Daehlin <simen.daehlin@strapi.io>
2024-06-12 11:15:24 +0100

    Renamed dashboard to courses for mocking

--- frontend/mocking/courses.ts
+++ frontend/mocking/courses.ts
@@ -1,10 +1,21 @@
 import { Course } from "@interfaces/course";
 import { faker } from "@faker-js/faker";
 
+export const categories = [
+  "Category 1",
+  "Category 2",
+  "Category 3",
+  "Category 4",
+  "Category 5",
+  "Category 6",
+];
+
 export function generateCourses(count: number = 5): Course[] {
   const courses: Course[] = [];
   for (let i = 0; i < count; i++) {
-    const randomImageNumber = faker.datatype.number({ min: 1, max: 1000 });
+    const randomImageNumber = faker.number.int({ min: 1, max: 1000 });
+    const randomCategory =
+      categories[faker.number.int({ min: 0, max: categories.length - 1 })];
     courses.push({
       id: faker.string.uuid(),
       title: faker.lorem.words(3),
@@ -12,6 +23,7 @@ export function generateCourses(count: number = 5): Course[] {
       photo: `https://placedog.net/${randomImageNumber}`,
       progress: faker.number.int({ min: 0, max: 100 }),
       subtitle: faker.lorem.words(3),
+      category: randomCategory,
     });
   }
   return courses;
