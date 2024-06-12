1c59ac80357c0c3b83f74412513eb1044322eeee
Simen Daehlin <simen.daehlin@strapi.io>
2024-06-12 13:00:14 +0100

    Added courseID

--- frontend/mocking/courses.ts
+++ frontend/mocking/courses.ts
@@ -1,4 +1,4 @@
-import { Course } from "@interfaces/course";
+import { Chapter, Course, Module } from "@interfaces/course";
 import { faker } from "@faker-js/faker";
 
 export const categories = [
@@ -16,6 +16,26 @@ export function generateCourses(count: number = 5): Course[] {
     const randomImageNumber = faker.number.int({ min: 1, max: 1000 });
     const randomCategory =
       categories[faker.number.int({ min: 0, max: categories.length - 1 })];
+
+    const sections: Chapter[] = [];
+    const sectionCount = faker.number.int({ min: 1, max: 5 });
+    for (let j = 0; j < sectionCount; j++) {
+      const checklists: Module[] = [];
+      const checklistCount = faker.number.int({ min: 1, max: 10 });
+      for (let k = 0; k < checklistCount; k++) {
+        checklists.push({
+          id: faker.string.uuid(),
+          title: faker.lorem.words(2),
+          completed: faker.datatype.boolean(),
+        });
+      }
+      sections.push({
+        id: faker.string.uuid(),
+        title: faker.lorem.words(3),
+        checklists: checklists,
+      });
+    }
+
     courses.push({
       id: faker.string.uuid(),
       title: faker.lorem.words(3),
@@ -24,6 +44,7 @@ export function generateCourses(count: number = 5): Course[] {
       progress: faker.number.int({ min: 0, max: 100 }),
       subtitle: faker.lorem.words(3),
       category: randomCategory,
+      sections: sections,
     });
   }
   return courses;
