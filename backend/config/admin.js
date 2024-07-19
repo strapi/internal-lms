53e46fcd796a4e806500fd43fcd694ad2fdf669a
Derrick Mehaffy <derrickmehaffy@gmail.com>
2024-07-19 08:14:06 -0700

    chore: more cleanup

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -22,8 +22,8 @@ module.exports = ({ env }) => ({
     },
   },
   flags: {
-    nps: env.bool("FLAG_NPS", true),
-    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
+    nps: env.bool("FLAG_NPS", false),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", false),
   },
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
