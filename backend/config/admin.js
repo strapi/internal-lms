cdafc049fd43b35c2388cd4e4b408a1006b05432
Derrick Mehaffy <derrickmehaffy@gmail.com>
2024-07-19 08:13:57 -0700

    chore: setup SSO and just general cleanup

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,17 +1,55 @@
+const GoogleStrategy = require("passport-google-oauth2");
+
+const verify = (request, accessToken, refreshToken, profile, done) => {
+  if (profile && profile.email && profile.email.endsWith("@strapi.io")) {
+    done(null, {
+      email: profile.email,
+      firstname: profile.given_name,
+      lastname: profile.family_name,
+    });
+  } else {
+    done(new Error("Forbidden email address"));
+  }
+};
+
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
+  },
+  auth: {
+    secret: env("ADMIN_JWT_SECRET"),
+    providers: [
+      {
+        uid: "google",
+        displayName: "Google",
+        icon: "https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Google-512.png",
+        createStrategy: (strapi) =>
+          // @ts-ignore
+          new GoogleStrategy(
+            {
+              clientID: env("GOOGLE_CLIENT_ID"),
+              clientSecret: env("GOOGLE_CLIENT_SECRET"),
+              scope: [
+                "https://www.googleapis.com/auth/userinfo.email",
+                "https://www.googleapis.com/auth/userinfo.profile",
+              ],
+              callbackURL:
+                env("PUBLIC_URL", "http://localhost:1337") +
+                strapi.admin.services.passport.getStrategyCallbackURL("google"),
+              passReqToCallback: true,
+            },
+            verify
+          ),
+      },
+    ],
   },
 });
