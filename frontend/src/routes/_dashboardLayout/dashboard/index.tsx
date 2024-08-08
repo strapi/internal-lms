fcedfb14b44930e91421d873b8a96bece0addbb8
bolg55 <kellen.bolger@strapi.io>
2024-08-08 16:06:58 -0400

    fix: add max-w container to dashboard

--- frontend/src/routes/_dashboardLayout/dashboard/index.tsx
+++ frontend/src/routes/_dashboardLayout/dashboard/index.tsx
@@ -10,7 +10,7 @@ export const Route = createFileRoute('/_dashboardLayout/dashboard/')({
 function Dashboard() {
   const courses = generateCourses(20);
   return (
-    <div className="space-y-16 p-4 md:p-8">
+    <div className="space-y-16 max-w-screen-2xl mx-auto p-4 md:p-8">
       <div className="space-y-8">
         <h2 className="text-center font-bold text-4xl mb-6">Courses in Progress</h2>
         <CourseCards courses={ courses.slice(0, 3) } showProgress />
