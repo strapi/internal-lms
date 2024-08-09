184752af00356ab4392e18e4492f05ddbd2aed0b
bolg55 <kellen.bolger@strapi.io>
2024-08-09 11:24:15 -0400

    feat: darkmode toggle

--- frontend/src/routes/_dashboardLayout.tsx
+++ frontend/src/routes/_dashboardLayout.tsx
@@ -1,27 +1,24 @@
-import { Input } from '@/components/ui/input';
-import UserNav from '@/components/UserNav';
-import { createFileRoute, Link, Outlet } from '@tanstack/react-router';
-import { FaBell, FaSearch } from 'react-icons/fa';
-import strapiLogo from '../assets/strapi-logo.svg';
-export const Route = createFileRoute('/_dashboardLayout')({
+import { Input } from "@/components/ui/input";
+import UserNav from "@/components/UserNav";
+import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
+import { FaBell, FaSearch } from "react-icons/fa";
+import strapiLogo from "../assets/strapi-logo.svg";
+import ThemeToggle from "@/components/ui/theme-toggle";
+export const Route = createFileRoute("/_dashboardLayout")({
   component: () => <DashboardLayout />,
 });
 
 export const DashboardLayout = () => {
-
   return (
     <div>
-      <div className="p-2 flex justify-between items-center m-2">
-        <div className="flex gap-2 items-center">
-          <Link to="/dashboard" className='w-8 h-8 mr-2'>
-            <img
-              src={ strapiLogo }
-              alt="Logo"
-            />
+      <div className="m-2 flex items-center justify-between p-2">
+        <div className="flex items-center gap-2">
+          <Link to="/dashboard" className="mr-2 h-8 w-8">
+            <img src={strapiLogo} alt="Logo" />
           </Link>
           <Link to="/auth" className="[&.active]:font-bold">
             Auth
-          </Link>{ ' ' }
+          </Link>{" "}
           <Link to="/dashboard" className="[&.active]:font-bold">
             Dashboard
           </Link>
@@ -29,19 +26,20 @@ export const DashboardLayout = () => {
             Courses
           </Link>
         </div>
-        { }
-        <div className="flex items-center gap-4 w-full justify-end">
+        {}
+        <div className="flex w-full items-center justify-end gap-4">
           <div className="relative w-1/3">
-            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10" />
+            <FaSearch className="absolute left-3 top-1/2 z-10 -translate-y-1/2 transform text-gray-500" />
             <Input
               type="text"
               placeholder="Search"
-              className="pl-10 pr-3 py-2 text-md w-full border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-[#6E23DD] focus:border-transparent"
+              className="text-md w-full rounded border border-gray-300 py-2 pl-10 pr-3 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#6E23DD]"
               value=""
             />
           </div>
-          <FaBell className='w-6 h-6' />
+          <FaBell className="h-6 w-6" />
           <UserNav />
+          <ThemeToggle />
         </div>
       </div>
       <hr />
@@ -50,4 +48,4 @@ export const DashboardLayout = () => {
       </main>
     </div>
   );
-}
+};
