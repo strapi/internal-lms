9c1ff3c60f0b0be9dc1b53356a1341b02ce7a490
bolg55 <kellen.bolger@strapi.io>
2024-08-16 11:22:07 -0400

    fix: remove devtools in prod

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,16 +1,25 @@
 import { AuthContextType } from "@/context/auth";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { lazy } from "react";
 
 interface RouterContext {
   auth: AuthContextType;
 }
 
+const TanStackRouterDevtools =
+  process.env.NODE_ENV !== "production"
+    ? lazy(() =>
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        })),
+      )
+    : () => null;
+
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
+      {TanStackRouterDevtools && <TanStackRouterDevtools />}
     </>
   ),
 });
