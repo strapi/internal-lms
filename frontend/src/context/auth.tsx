97a07d1a2d2ab515369d1560d89b8c636f4d1a5d
Richard <kasonde.nsama@gmail.com>
2024-08-23 16:02:02 +0200

    Added temporary default context just to fix null context error

--- frontend/src/context/auth.tsx
+++ frontend/src/context/auth.tsx
@@ -1,15 +1,19 @@
+import { AuthContextType } from "@/interfaces/auth";
 import { createContext } from "react";
 
 // TODO: Define the AuthContextType interface
-export interface AuthContextType {}
 
-export const AuthContext = createContext<AuthContextType | null>(null);
+const defaultState: AuthContextType = {
+  loggedIn: false,
+};
+
+export const AuthContext = createContext<AuthContextType>(defaultState);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   // TODO: Implement the AuthProvider component
 
   return (
     // TODO: Provide the AuthContext value(s)
-    <AuthContext.Provider value={null}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={defaultState}>{children}</AuthContext.Provider>
   );
 };
