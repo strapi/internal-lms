9e9bdeb402d95c3eadab4de943aa33eddbfbeb60
alex-strapi <alex.bennett@strapi.io>
2024-11-18 14:42:33 +0000

    Chore: Upgraded to Strapi 5

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "mux-video-uploader": {
+    enabled: true,
+    // resolve: './src/plugins/strapi-plugin-mux-video-uploader',
+    config: {
+      accessTokenId: env("ACCESS_TOKEN_ID"),
+      secretKey: env("ACCESS_TOKEN_SECRET"),
+    },
+  },
+});
