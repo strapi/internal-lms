5d1e376f7165fb04685be647a39d066674fa9243
DMehaffy <derrickmehaffy@gmail.com>
2024-12-30 16:28:25 -0800

    fix: vite.config.ts to define the Strapi URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,16 +7,21 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), tsconfigPaths()],
-  test: {
-    globals: true,
-    include: ["./tests/*.test.ts"],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@interfaces": path.resolve(__dirname, "./types")
+export default defineConfig(() => {
+  return {
+    define: {
+      __APP_ENV__: process.env.VITE_STRAPI_URL,
+    },
+    plugins: [react(), TanStackRouterVite(), tsconfigPaths()],
+    test: {
+      globals: true,
+      include: ["./tests/*.test.ts"],
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@interfaces": path.resolve(__dirname, "./types")
+      },
     },
   },
 });
